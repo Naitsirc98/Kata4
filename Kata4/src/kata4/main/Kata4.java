588fd6fcac1c7920b977d835ed205217e26d4324
Cristian Herrera <naitsirc98@gmail.com>
2018-11-08 15:42:00 +0000

    Version 2 (CIPO)

--- Kata4/src/kata4/main/Kata4.java
+++ Kata4/src/kata4/main/Kata4.java
@@ -1,23 +1,53 @@
 package kata4.main;
 
+import java.util.List;
+
 import kata4.model.Histogram;
+import kata4.model.Mail;
 import kata4.view.HistogramDisplay;
 import kata4.view.MailHistogramBuilder;
 import kata4.view.MailListReader;
 
 public class Kata4 {
-
+	
 	public static void main(String[] args) {
 		
-		final String filename = "email.txt";
+		Kata4 kata = new Kata4("email.txt");
 		
-		Histogram<String> histogram = MailHistogramBuilder
-				.build(MailListReader.read(filename));
-		
-		HistogramDisplay display = new HistogramDisplay(histogram);
-		
-		display.execute();
+		kata.execute();
 
+	}
+	
+	private final String filename;
+	private List<Mail> mails;
+	private Histogram<String> histogram;
+	private HistogramDisplay display;
+
+	public Kata4(final String filename) {
+		this.filename = filename;
+	}
+	
+	private void execute() {
+		
+		input();
+		
+		process();
+		
+		output();
+		
+	}
+	
+	private void input() {
+		mails = MailListReader.read(filename);
+	}
+	
+	private void process() {
+		histogram = MailHistogramBuilder.build(mails);
+	}
+	
+	private void output() {
+		display = new HistogramDisplay(histogram);
+		display.execute();
 	}
 
 }
